cdc4c0b3732503aaedc550d18467c032ab73d17b
nithishganesan17 <128446809+nithishganesan17@users.noreply.github.com>
2026-08-22 09:26:56 +0530

    Sumofarray.java

--- Arrays/Sumofarray.java
+++ Arrays/Sumofarray.java
@@ -1,1 +1,12 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+      int[]n={1,2,3,4};
+      int a=0;
+      for(int i=0;i<n.length;i++){
+         a=n[i]+a;
+      }
+      System.out.print(a);
+    }
+}
