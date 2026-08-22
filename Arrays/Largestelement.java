9f2874e79958ecef6a772f9d1cad13b3c05928b7
nithishganesan17 <128446809+nithishganesan17@users.noreply.github.com>
2026-08-22 09:57:11 +0530

    Update Largestelement.java

--- Arrays/Largestelement.java
+++ Arrays/Largestelement.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int[]n=new int[a];
+      for(int i=0;i<a;i++){
+        n[i]=sc.nextInt();
+      }
+      int large=n[0];
+      for(int i=1;i<a;i++){
+      if(n[i]>large){
+        large=n[i];
+      }
+    }
+      System.out.print(large);
+  }
+}
