a1cfb500586b30c062f3de6c660f52b93e721e2d
nithishganesan17 <128446809+nithishganesan17@users.noreply.github.com>
2026-08-23 18:02:12 +0530

    Create Duplicate element.java

---
+++ Arrays/Duplicate element.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int[]n=new int[a];
+      for(int i=0;i<a;i++){
+        n[i]=sc.nextInt();
+        int count=0;
+        for(int j=0;j<i;j++){
+          if(n[i]==n[j]){
+            count++;
+          }
+        }
+      if(count>0){
+        System.out.print(n[i]);
+      }
+      }
+    }
+}
